8c3ab3fbf15e1c17aa295f62e2a0294023a85f3f
Danilo <danilohsaraiva@gmail.com>
2026-06-13 22:02:43 -0300

    feat(users): add create user DTO validation

--- src/GrowStore.Application/Users/DTOs/CreateUserDto.cs
+++ src/GrowStore.Application/Users/DTOs/CreateUserDto.cs
@@ -4,6 +4,7 @@ namespace GrowStore.Application.Users.DTOs;
     public class CreateUserDto
     {
         public string Name { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
         public UserRole Role { get; set; }
     }
--- src/GrowStore.Application/Users/DTOs/ResponseUserDto.cs
+++ src/GrowStore.Application/Users/DTOs/ResponseUserDto.cs
@@ -5,6 +5,7 @@ namespace GrowStore.Application.Users.DTOs;
     {
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
         public UserRole Role { get; set; }
     }

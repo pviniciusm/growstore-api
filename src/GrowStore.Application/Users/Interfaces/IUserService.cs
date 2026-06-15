6333fe3e59f0e672b81ff3ea6de1c93cd0a93741
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-06-15 14:20:02 -0300

    feat: add IUserService interface

---
+++ src/GrowStore.Application/Users/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using GrowStore.Application.Users.DTOs;
+
+namespace GrowStore.Application.Users.Interfaces
+{
+    public interface IUserService
+    {
+        Task<ResponseUserDto> CreateUserAsync(CreateUserDto createUserDto);
+        Task<ResponseUserDto> GetUserByIdAsync(Guid id);
+        Task UpdateUserAsync(Guid id, UpdateUserDto updateUserDto);
+        Task DeleteUserAsync(Guid id);
+    }
+}

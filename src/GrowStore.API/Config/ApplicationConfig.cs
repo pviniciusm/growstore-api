e6d4d60f2cf51eef4076b9178d9bf6d52c9cda91
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-06-15 14:20:42 -0300

    feat: add DI config for application services and repositories

---
+++ src/GrowStore.API/Config/ApplicationConfig.cs
@@ -0,0 +1,15 @@
+﻿using GrowStore.Application.Users.Interfaces;
+using GrowStore.Application.Users.Services;
+
+namespace GrowStore.API.Config
+{
+    public static class ApplicationConfig
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}

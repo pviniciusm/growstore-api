36a4f8c3fb93c9e7538143f64f860a51542a56a1
fabricio-milanio <milanio.fabricio@gmail.com>
2026-06-16 17:28:18 -0300

    :sparkles: feat(application): dto adress attributies adjusted to english

--- src/GrowStore.Application/Address/DTOs/ResponseAddressDto.cs
+++ src/GrowStore.Application/Address/DTOs/ResponseAddressDto.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace GrowStore.Application.Addresses.DTOs;
 
-public class ResponseAddressDto
+public class AddressResponseDto
 {
     public Guid Id { get; set; }
-    public string Logradouro { get; set; } = string.Empty;
-    public string Numero { get; set; } = string.Empty;
-    public string? Complemento { get; set; }
-    public string Bairro { get; set; } = string.Empty;
-    public string Cidade { get; set; } = string.Empty;
-    public string Estado { get; set; } = string.Empty;
-    public string Cep { get; set; } = string.Empty;
+    public string Street { get; set; } = string.Empty;
+    public string Number { get; set; } = string.Empty;
+    public string? Complement { get; set; }
+    public string Neighborhood { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
 }

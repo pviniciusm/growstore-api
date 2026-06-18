56d3d529e8bb9ca55dd942cb6777e127a9cd54c6
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-06-17 21:46:05 -0300

    fix whitespace and formatting

--- src/GrowStore.Application/Auth/Interfaces/IAuthService.cs
+++ src/GrowStore.Application/Auth/Interfaces/IAuthService.cs
@@ -1,9 +1,8 @@
 namespace GrowStore.Application.Auth.Interfaces
 {
-
     public interface IAuthService
     {
         Task<string> LoginAsync(string email, string password);
         Task<string> GenerateTestToken();
-    }   
+    }
 }
--- src/GrowStore.Domain/Interfaces/IAuthRepository.cs
+++ src/GrowStore.Domain/Interfaces/IAuthRepository.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace GrowStore.Domain.Interfaces
 {
     public interface IAuthRepository

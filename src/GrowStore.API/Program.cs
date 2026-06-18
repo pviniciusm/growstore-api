6aa81274b84097f9bf880be10db156702aba477d
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-06-18 11:18:52 -0300

    feat: add JwtSettings config class bound from appsettings.dev

--- src/GrowStore.API/Program.cs
+++ src/GrowStore.API/Program.cs
@@ -1,4 +1,5 @@
 using GrowStore.API.Config;
+using GrowStore.Application.Auth.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
 builder.Services.AddApplicationServices();
 builder.Services.AddRepositories();
 

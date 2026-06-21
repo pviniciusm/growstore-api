e23fc5d4f3875a23305e1fcf64e037f22c91dd2e
jfan.dev <jfan.dev@gmail.com>
2026-06-21 12:09:38 -0300

    🔧 chore: solve merge conflict

--- src/GrowStore.API/Program.cs
+++ src/GrowStore.API/Program.cs
@@ -1,4 +1,5 @@
 using GrowStore.API.Config;
+using GrowStore.API.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,8 @@ builder.Services.AddApplicationServices();
 builder.Services.AddRepositories();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

3570f0c46a73173c49caf9bd70a225cb6fc1fb0e
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-07-01 16:54:47 -0300

    feat: add generic PagedResult<T> DTO

---
+++ src/GrowStore.Application/Common/Pagination/PagedResult.cs
@@ -0,0 +1,19 @@
+﻿namespace GrowStore.Application.Common.Pagination;
+
+public class PagedResult<T>
+{
+    public IEnumerable<T> Items { get; init; } = [];
+    public int Page { get; init; }
+    public int Limit { get; init; }
+    public int TotalCount { get; init; }
+    public int TotalPages => Limit > 0 ? (int)Math.Ceiling(TotalCount / (double)Limit) : 0;
+
+    public static PagedResult<T> Create(IEnumerable<T> items, int page, int limit, int totalCount)
+        => new()
+        {
+            Items = items,
+            Page = page,
+            Limit = limit,
+            TotalCount = totalCount
+        };
+}

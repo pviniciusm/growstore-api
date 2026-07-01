95c341b162d41fb11e6d24cd96f6d39d388ac522
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-07-01 14:33:29 -0300

    feat: add Address validators and validation rules

---
+++ src/GrowStore.Application/Addresses/Validators/CreateAddressDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using GrowStore.Application.Addresses.DTOs;
+using GrowStore.Application.Shared.Rules;
+
+namespace GrowStore.Application.Addresses.Validators;
+
+public class CreateAddressDtoValidator : AbstractValidator<CreateAddressDto>
+{
+    public CreateAddressDtoValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("User ID is required.");
+
+        RuleFor(x => x.Street)
+            .NotEmpty().WithMessage("Street is required.")
+            .MaximumLength(200).WithMessage("Street must be at most 200 characters long.");
+
+        RuleFor(x => x.City)
+            .NotEmpty().WithMessage("City is required.")
+            .MaximumLength(100).WithMessage("City must be at most 100 characters long.");
+
+        RuleFor(x => x.State)
+            .NotEmpty().WithMessage("State is required.")
+            .MaximumLength(100).WithMessage("State must be at most 100 characters long.");
+
+        RuleFor(x => x.ZipCode)
+            .NotEmpty().WithMessage("Zip code is required.")
+            .MaximumLength(20).WithMessage("Zip code must be at most 20 characters long.")
+            .Must(AddressValidationRules.BeAValidZipCode).WithMessage("The provided zip code is invalid.");
+
+        RuleFor(x => x.Reference)
+            .MaximumLength(100).WithMessage("Reference must be at most 100 characters long.")
+            .When(x => x.Reference is not null);
+    }
+}

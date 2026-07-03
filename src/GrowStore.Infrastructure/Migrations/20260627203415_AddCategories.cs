7a6c9f4039efa87ae3bc1db88285299b0610da39
Vinícius Hiago Martins <viniciushiagomartins00@gmail.com>
2026-07-03 10:46:12 -0300

    chore: add ExcludeFromCodeCoverage to migrations

--- src/GrowStore.Infrastructure/Migrations/20260627203415_AddCategories.cs
+++ src/GrowStore.Infrastructure/Migrations/20260627203415_AddCategories.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
 
 #nullable disable
 
 namespace GrowStore.Infrastructure.Migrations
 {
+    [ExcludeFromCodeCoverage]
     /// <inheritdoc />
     public partial class AddCategories : Migration
     {
